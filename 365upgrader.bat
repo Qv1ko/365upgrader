2fe7a241ebcc468e2d35c98d5730229db0fc4122
Víctor García <vicgarmur947@gmail.com>
2024-04-22 18:05:38 +0100

    Added the Windows updates section and title as well as configuring the opening of the platforms to be updated.

--- 365upgrader.bat
+++ 365upgrader.bat
@@ -10,133 +10,214 @@ setlocal enabledelayedexpansion
 		)
 
 		if '%errorlevel%' neq '0' (
-		    goto UACPrompt
+			goto UAC
 		) else (
 			goto gotAdmin
 		)
 
-		:UACPrompt
-	    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-	    set params= %*
-	    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-	    "%temp%\getadmin.vbs"
-	    del "%temp%\getadmin.vbs"
-	    exit /b
+		:UAC
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+		set params= %*
+		echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+		"%temp%\getadmin.vbs"
+		del "%temp%\getadmin.vbs"
+		exit /b
 
 		:gotAdmin
 		pushd "%CD%"
 		cd /d "%~dp0"
 
-	rem Windows Update
-		echo [+] Windows Update finished
-		rem for %%W in (*.msu) do (
-		rem     echo: Installing update: %%#
-		rem     Wusa "%%W" /quiet /norestart
-		rem )
-
+	echo .----.  .-. .----..-..-----..-..-.      .-.     .-..-.                           .-.           
+	echo `--  ; .'.' : .--': :`-. .-': :; :      : :     : :: :                           : :           
+	echo  .' ' .' '. `. `. : :  : :  :    :.-..-.: `-.   : :: :.---.  .--. .--.  .--.   .-' : .--. .--. 
+	echo  _`,`.: .; :.-`, :: :  : :  : :: :: :; :' .; :  : :; :: .; `' .; :: ..'' .; ; ' .; :' '_.': ..'
+	echo `.__.'`.__.'`.__.':_;  :_;  :_;:_;`.__.'`.__.'  `.__.': ._.'`._. ;:_;  `.__,_;`.__.'`.__.':_;  
+	echo                                                       : :    .-. :                             
+	echo                                                       :_;    `._.'                             
+	echo.
 	echo.
 
-	rem Detect drives
+	rem Upgrade Windows
+		echo [#] Checking for Windows updates...
+		powershell -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned" > nul || goto :updateError
+		powershell -command "Install-PackageProvider -Name NuGet -Force" > nul || goto :updateError
+		powershell -command "Install-Module -Name PSWindowsUpdate -Force" > nul || goto :updateError
+		powershell -command "Import-Module PSWindowsUpdate" > nul || goto :updateError
+		powershell -command "Install-WindowsUpdate -MicrosoftUpdate -AcceptAll" > nul || goto :updateError
+		powershell -command "Set-ExecutionPolicy -ExecutionPolicy Undefined" > nul || goto :updateError
+		echo     [+] Windows update finished
+		goto :endUpgradeWindows
+
+		:updateError
+		echo [-] Error trying to upgrade Windows
+
+		:endUpgradeWindows
+		
+	echo.
+
+	rem Drive finder
 		set "drives[0]= "
 		set "num=0"
 
-		for /f "tokens=2 delims==" %%a in ('wmic logicaldisk get caption /value') do (
-		    set "drive=%%a"
-		    set "drive=!drive:~0,-1!\"
-		    set "drives[!num!]=!drive!"
-		    set /a num+=1
+		for /f "tokens=2 delims==" %%v in ('wmic logicaldisk get caption /value') do (
+			set "drive=%%v"
+			set "drive=!drive:~0,-1!\"
+			set "drives[!num!]=!drive!"
+			set /a num+=1
 		)
 
 		set /a num-=1
 
 	rem Geforce Experience
-		for /R C:\ %%X in ("NVIDIA~1.EXE") do (
-		    if exist "%%X" (
-		        echo [#] Starting NVIDIA GeForce Experience
-		        start "" "%%X"
-		        goto :steam
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching NVIDIA GeForce Experience in the !drives[%%n]! drive...
+			call :searchGeforceExperience
 		)
 
-	rem Steam
-		:steam
+		:searchGeforceExperience
+		for /R "%d%" %%g in (NVIDIA~1.EXE) do (
+			if exist "%%g" (
+				echo     [+] Starting NVIDIA GeForce Experience
+				start "" "%%g"
+				goto :endGeforceExperience
+			)
+		)
+		echo [-] NVIDIA GeForce Experience was not found on %d% drive
+		goto :eof
 
-		for /l %%i in (0,1,%num%) do (
-		    set "r=!drives[%%i]!"
-		    echo [#]Finding Steam in !drives[%%i]! drive...
-		    
-		    call :searchSteam
+		:endGeforceExperience
+
+	rem Steam
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching Steam in the !drives[%%n]! drive...
+			call :searchSteam
 		)
 
 		:searchSteam
-		for /R "%r%" %%X in ("steam.exe") do (
-		    if exist "%%X" (
-		        echo [+] Starting Steam
-		        start "" "%%X"
-		        goto :battlenet
-		    )
+		for /R "%d%" %%g in (steam.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting Steam
+				start "" "%%g"
+				goto :endSteam
+			)
 		)
+		echo [-] Steam was not found on %d% drive
+		goto :eof
 
-		goto :eof	
-
+		:endSteam
 
 	rem Battle.net
-		:battlenet
-		for /R D:\ %%X in ("Battle.net.exe") do (
-		    if exist "%%X" (
-		        echo [#] Starting Battle.net
-		        start "" "%%X"
-		        goto :lolIni
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching Battle.net in the !drives[%%n]! drive...
+			call :searchBattlenet
 		)
+
+		:searchBattlenet
+		for /R "%d%" %%g in (Battle.net.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting Battle.net
+				start "" "%%g"
+				goto :endBattlenet
+			)
+		)
+		echo [-] Battle.net was not found on %d% drive
+		goto :eof
+
+		:endBattlenet
 
 	rem League of Legends
-		:lolIni
-		for /R D:\ %%X in ("RiotClientServices.exe") do (
-		    if exist "%%X" (
-		        echo [#] Starting League of Legends
-		        start "" "%%X" --launch-product=league_of_legends --launch-patchline=live
-		        goto :epicIni
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching League of Leguends in the !drives[%%n]! drive...
+			call :searchLoL
 		)
+
+		:searchLoL
+		for /R "%d%" %%g in (RiotClientServices.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting League of Leguends
+				start "" "%%g" --launch-product=league_of_legends --launch-patchline=live
+				goto :endLoL
+			)
+		)
+		echo [-] League of Leguends was not found on %d% drive
+		goto :eof
+
+		:endLoL
 
 	rem Epic Games
-		:epicIni
-		for /R D:\ %%X in ("EpicGamesLauncher.exe") do (
-		    if exist "%%X" (
-		        echo [#] Starting Epic Games
-		        start "" "%%X"
-		        goto :eaIni
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching Epic Games in the !drives[%%n]! drive...
+			call :searchEpicGames
 		)
+
+		:searchEpicGames
+		for /R "%d%" %%g in (EpicGamesLauncher.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting Epic Games
+				start "" "%%g"
+				goto :endEpicGames
+			)
+		)
+		echo [-] Epic Games was not found on %d% drive
+		goto :eof
+
+		:endEpicGames
 
 	rem EA
-		:eaIni
-		for /R C:\ %%X in ("EALauncher.exe") do (
-		    if exist "%%X" (
-		        echo [#] Starting EA
-		        start "" "%%X"
-		        goto :valorantIni
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching EA in the !drives[%%n]! drive...
+			call :searchEA
 		)
+
+		:searchEA
+		for /R "%d%" %%g in (EALauncher.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting EA
+				start "" "%%g"
+				goto :endEA
+			)
+		)
+		echo [-] EA was not found on %d% drive
+		goto :eof
+
+		:endEA
+
+	echo.
+	echo [?] Press any key to open Valorant...
+	pause > nul
+	echo.
 
 	rem Valorant
-		:valorantIni
-
-		echo.
-
-		echo [?] Press any key to open Valorant...
-		pause > nul
-
-		echo.
-
-		for /R D:\ %%X in ("RiotClientServices.exe") do (
-		    if exist "%%X" (
-		        echo [#] Starting Valorant
-		        start "" "%%X" --launch-product=valorant --launch-patchline=live
-		        goto :clearIni
-		    )
+		for /l %%n in (0,1,%num%) do (
+			set "d=!drives[%%n]!"
+			echo [#] Searching Valorant in the !drives[%%n]! drive...
+			call :searchValorant
 		)
+
+		:searchValorant
+		for /R "%d%" %%g in (RiotClientServices.exe) do (
+			if exist "%%g" (
+				echo     [+] Starting Valorant
+				start "" "%%g" --launch-product=valorant --launch-patchline=live
+				goto :endValorant
+			)
+		)
+		echo [-] Valorant was not found on %d% drive
+		goto :eof
+
+		:endValorant
+
+	rem Delete download files
+
+		del /s /f /q %userprofile%\Downloads\*.*
+		rd /s /q %userprofile%\Downloads
+		md %userprofile%\Downloads > nul
 
 	rem Delete temp files
 		:clearIni
@@ -167,7 +248,8 @@ setlocal enabledelayedexpansion
 
 	echo.
 
-	echo [!] Tasks are finished, press a key to exit...
+	echo [?] Tasks are finished, press a key to exit...
 	pause > nul
+	doskey /listsize=0
 
 endlocal
